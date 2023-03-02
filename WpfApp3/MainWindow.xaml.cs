091dfa127b1ba75075b17942e48bc9097f5b69a3
Seyidmammadli_i <Seyidmammadli_i@STHQ012E-01>
2023-03-02 11:51:10 +0400

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Button btn = new Button
+            //{
+            //    Content = "7",
+            //    FontSize = 24.1,
+            //    Width = 100,
+            //    Height = 35,
+            //    Margin = new Thickness(5)
+            //};
+
+            //sp.Children.Add(btn);
+            
+
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    sp.Children.Remove(btn6);
+        //}
+    }
+}
